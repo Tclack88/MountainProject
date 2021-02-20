49576bd84e0a103cd11e87a06cb2ec2c0d352fcc
Trevor Clack <tclack@aol.com>
2021-02-19 21:54:07 -0800

    created quick error checker script for hangboard. Used this to fix two entries

---
+++ hangboard_error_check.py
@@ -0,0 +1,32 @@
+import datetime as dt
+import pandas as pd
+import numpy as np
+import sys
+
+doc = sys.argv[1]
+
+df = pd.read_excel(doc)
+df.date = pd.to_datetime(df.date)
+df.date = pd.to_datetime(df.date, format='%d %b %Y')
+df = df.set_index('date')
+
+def error_check(entry, expected=6):
+    entry_list = list(map(float,entry.split(',')))
+    success = entry_list[1]
+    fail = len(entry_list) - 2
+    total = success + fail
+    if total != expected:
+        return True
+    return False
+
+problems = df.applymap(lambda x: error_check(x))
+indices = problems.index.to_list()
+locations = [(indices[x], problems.columns[y]) for x,y in zip(*np.where(problems.values == True))] # get "coordinates" i.e. row, column where "True" exists
+
+print('Mistakes-- check these:')
+if locations:
+    for date, hold in locations:
+        print(f'{date.to_pydatetime().strftime("%d%b%Y")} : {hold}')
+
+else:
+    print('No errors found')
