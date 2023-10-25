bd00e2483b53fcef535294e3b8922236d15def62
Tclack88 <tclack@aol.com>
2023-10-25 12:50:33 -0700

    update hangboard entries as well as error check to account for any erros in the one-handed entries

--- hangboard_error_check.py
+++ hangboard_error_check.py
@@ -11,13 +11,21 @@ df.date = pd.to_datetime(df.date, format='%d %b %Y')
 df = df.set_index('date')
 
 def error_check(entry, expected=6): # expected is number of total rounds
-    entry_list = list(map(float,entry.split(',')))
-    success = entry_list[1]
-    fail = len(entry_list) - 2
-    total = success + fail
-    if total != expected:
-        return True
-    return False
+    def check_element(elem):
+      entry_list = list(map(float,elem.split(',')))
+      success = entry_list[1]
+      fail = len(entry_list) - 2
+      total = success + fail
+      if total != expected:
+          return True
+      return False
+    if ':' in entry:
+        left,right = entry.split(':')
+        left_res = check_element(left[2:])
+        right_res = check_element(right[2:])
+        return left_res or right_res
+    else:
+      check_element(entry)
 
 problems = df.applymap(lambda x: error_check(x,6))
 indices = problems.index.to_list()
